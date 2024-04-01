5691557a1e29a2b83cd8c520a16004bbbabda7cf
manmoth <944769+manmoth@users.noreply.github.com>
2024-04-01 17:09:06 +0200

    feat: add game tracking page

--- TmTracker/Program.cs
+++ TmTracker/Program.cs
@@ -1,10 +1,13 @@
 using TmTracker.Components;
+using TmTracker.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<TrackedGameService>();
 
 var app = builder.Build();
 
